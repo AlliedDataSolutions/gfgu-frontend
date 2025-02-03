3e4d114b6d2013fa12dd16bbfcc79565ade36927
Vivekt719 <114002370+Vivekt719@users.noreply.github.com>
2025-02-02 23:59:37 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/tailwind.css'
-import { ProfileForm } from './app/testForm.tsx'
+import { ProfileForm } from './app/TestForm.tsx'
 // import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
