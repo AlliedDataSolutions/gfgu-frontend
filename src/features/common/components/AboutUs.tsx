33df56bd25be74ed7cd8fe306f4a3c21f5739d0b
Yussuf <106894981+tayojs@users.noreply.github.com>
2025-02-20 12:47:50 -0700

    Fix Deployment issue

--- src/features/common/components/AboutUs.tsx
+++ src/features/common/components/AboutUs.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import { Button } from "@/components/ui/button";
 import elipses from "@/assets/elipses.png";
 import rect9 from "@/assets/rect9.png";
