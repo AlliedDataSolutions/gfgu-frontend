a71a3b931c2d44d0f660910257470bb341aafac9
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-03-04 14:08:23 -0700

    implement register and login

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-normal transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-brand-950 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:focus-visible:ring-brand-300",
@@ -17,8 +17,11 @@ const buttonVariants = cva(
           "border border-brand-200 bg-white shadow-sm hover:bg-brand-100 hover:text-brand-900 dark:border-brand-800 dark:bg-brand-950 dark:hover:bg-brand-800 dark:hover:text-brand-50",
         secondary:
           "bg-brand-50 text-brand-700 shadow-sm hover:bg-brand-50/80 border border-brand-700 dark:bg-brand-800 dark:text-brand-50 dark:hover:bg-brand-800/80",
-        ghost: "hover:bg-brand-100 hover:text-brand-900 dark:hover:bg-brand-800 dark:hover:text-brand-50",
+        ghost:
+          "hover:bg-brand-100 hover:text-brand-900 dark:hover:bg-brand-800 dark:hover:text-brand-50",
         link: "text-neutral-900 underline-offset-4 hover:underline dark:text-brand-50",
+        disabled:
+          "bg-neutral-200 text-neutral-900 shadow-none",
       },
       size: {
         default: "h-9 px-4 py-2",
@@ -32,26 +35,26 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : "button"
+    const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Button.displayName = "Button"
+);
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
--- src/features/auth/pages/Register.tsx
+++ src/features/auth/pages/Register.tsx
@@ -1,3 +1,157 @@
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio";
+import { Input } from "@/components/ui/input";
+import { Circle, Eye, EyeOff } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import Vegies from "@/assets/vegies.png";
+
 export const Register = () => {
-    return <div>Register Page</div>;
-}
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [accountType, setAccountType] = React.useState("customer");
+  const [agreed, setAgreed] = React.useState(false);
+
+  return (
+    
+    <div className="flex h-screen ">
+      {/* Left side - Form (scrollable) */}
+      <div className="flex-1 overflow-y-auto p-6 md:p-10 lg:p-16">
+        <div className="mx-auto max-w-md">
+          <h1 className="mb-2 text-center text-3xl font-semibold">
+            Create Account
+          </h1>
+          <p className="mb-8 text-center text-gray-600">
+            Create an account to buy, sell, and connect with trusted
+            agricultural partners
+          </p>
+        </div>
+
+        {/* Account Type Selection */}
+        <div className="flex justify-center space-x-6 mt-10">
+          <RadioGroup
+            className="grid-cols-2 gap-8 text-gray-600"
+            value={accountType}
+            onValueChange={setAccountType}
+          >
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="customer"
+                id="customer"
+                checked={accountType === "customer"}
+              />
+              <Label htmlFor="customer">Customer</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem
+                value="vendor"
+                id="vendor"
+                checked={accountType === "vendor"}
+              />
+              <Label htmlFor="vendor">Vendor</Label>
+            </div>
+          </RadioGroup>
+        </div>
+
+        {/* Form Fields */}
+        <div className="space-y-4 mx-auto max-w-xl mt-10">
+          <div className="flex gap-4 flex-col sm:flex-row">
+            <div className="flex-1">
+              <Label htmlFor="firstName">First name</Label>
+              <Input
+                type="text"
+                id="firstName"
+                placeholder="Your first name"
+              ></Input>
+            </div>
+
+            <div className="flex-1">
+              <Label htmlFor="lastName">Last name</Label>
+              <Input
+                type="text"
+                id="lastName"
+                placeholder="Your last name"
+              ></Input>
+            </div>
+          </div>
+
+          <div>
+            <Label htmlFor="email">Email Address</Label>
+            <Input
+              type="email"
+              id="email"
+              placeholder="Your email address"
+            ></Input>
+          </div>
+
+          <div>
+            <Label htmlFor="phone">Phone Number</Label>
+            <Input type="tel" id="phone" placeholder="Enter number"></Input>
+          </div>
+
+          {accountType === "vendor" && (
+            <div>
+              <Label htmlFor="businessName">Business Name</Label>
+              <Input
+                type="text"
+                id="businessName"
+                placeholder="Enter name"
+              ></Input>
+            </div>
+          )}
+
+          <div>
+            <Label htmlFor="password">Password</Label>
+
+            <Input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter password"
+              icon={showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              onIconClick={() => setShowPassword((prev) => !prev)}
+            />
+            <div className="flex justify-between text-sm p-2">
+              <div className="flex items-center space-x-4">
+                <Circle size={12} /> <span>Use 8 or more characters</span>
+              </div>
+
+              <div className="flex items-center space-x-4">
+                <Circle size={12} /> <span>Use a symbol (e.g. !@#$)</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center space-x-4 p-2">
+            <Checkbox
+              checked={agreed}
+              onCheckedChange={() => {
+                setAgreed(!agreed);
+              }}
+              id="termsAndCond"
+            ></Checkbox>
+            <Label htmlFor="termsAndCond">
+              By continuing you agree to the Agric{" "}
+              <Link className="underline" to={""}>
+                terms of service
+              </Link>{" "}
+              and{" "}
+              <Link className="underline" to={""}>
+                privacy policy
+              </Link>
+              .
+            </Label>
+          </div>
+
+          <Button className="w-full" variant={"disabled"} size="lg">
+            Continue
+          </Button>
+        </div>
+      </div>
+
+      {/* banner */}
+      <div className="hidden h-screen w-1/3 overflow-hidden md:block">
+        <img className="h-full w-full object-cover" src={Vegies} alt="" />
+      </div>
+    </div>
+  );
+};
