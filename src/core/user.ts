3c4fb7536d8a5db1f72dec665f47c713c0e103ba
Yussuf <106894981+tayojs@users.noreply.github.com>
2025-03-16 10:36:30 -0600

    Add vendor interface and vendor property

--- src/core/user.ts
+++ src/core/user.ts
@@ -1,11 +1,19 @@
 import { Role } from "./role";
 
+export interface Vendor {
+  id: string;
+  businessName: string;
+  vendor: Vendor;
+}
+
+
 export interface User {
   id: string;
   firstName: string;
   lastName: string;
   email: string;
   role: Role;
+  vendor: Vendor;
   phoneNumber?: string;
   createdDate: Date;
   modifiedDate: Date;
