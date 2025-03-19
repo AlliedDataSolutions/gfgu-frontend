643c2bba10d7145455acef64be43590278a5df1c
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-03-18 20:19:11 -0600

    consume order endpoints

--- src/features/store/pages/ProductListing.tsx
+++ src/features/store/pages/ProductListing.tsx
@@ -9,8 +9,11 @@ import { useProductFilter } from "../hooks/useProductFilter";
 import { deliveryDays, menuItems, storeMenuItems } from "@/core/data";
 import Marquee from "react-fast-marquee";
 import { DeliveryDayComp } from "@/features/common/components/HeroSection";
+import { useCartContext } from "../hooks/CartContext";
+import InlineLoader from "@/components/ui/inlineloading";
 
 export default function ProductListing() {
+  const { loading } = useCartContext();
   interface Filters {
     categories: string[];
     vendors: string[];
@@ -48,81 +51,82 @@ export default function ProductListing() {
     appliedFilters.priceRange[1]
   );
 
+  if (loading) {
+    return <InlineLoader loading={loading} children={<div></div>} />;
+  }
+
   return (
     <>
-      <Header menuItems={storeMenuItems} />
+        <Header menuItems={storeMenuItems} />
 
-      {/* delivery days */}
-      <div className="bg-black py-4 mt-16">
-        <Marquee speed={50}>
-          {deliveryDays.map((item, index) => (
-            <DeliveryDayComp
-              key={index}
-              location={item.location}
-              dayOfWeek={item.dayOfWeek}
-            />
-          ))}
-        </Marquee>
-      </div>
-
-      <div className="container mx-auto py-8 px-4 mt-4 sm:mt-8">
-        <div className="flex flex-col sm:flex-row space-y-4 sm:justify-between sm:items-center mb-6">
-          <div>
-            <h1 className="text-2xl">Showing all products</h1>
-            <p className="text-sm text-muted-foreground">
-              Fresh from the farm to your table
-            </p>
-          </div>
-          <div className="flex items-center gap-2">
-            <span className="text-sm">Sort by:</span>
-            <select
-              className="border rounded-md px-2 py-1 text-sm"
-              value={sortBy}
-              onChange={handleSortChange}
-            >
-              <option value="newest">Newest Products</option>
-              <option value="price-low-high">Price: Low to High</option>
-              <option value="price-high-low">Price: High to Low</option>
-              <option value="name-a-z">Name: A to Z</option>
-            </select>
-          </div>
+        {/* delivery days */}
+        <div className="bg-black py-4 mt-16">
+          <Marquee speed={50}>
+            {deliveryDays.map((item, index) => (
+              <DeliveryDayComp
+                key={index}
+                location={item.location}
+                dayOfWeek={item.dayOfWeek}
+              />
+            ))}
+          </Marquee>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="w-full md:w-64 shrink-0 border px-6 pt-6 pb-20 rounded-md">
-            <FilterSidebar
-              filters={pendingFilters}
-              onFilterChange={setPendingFilters}
-            />
-            <Button onClick={applyFilters} className="w-full mt-4">
-              Apply Filters
-            </Button>
+        <div className="container mx-auto py-8 px-4 mt-4 sm:mt-8">
+          <div className="flex flex-col sm:flex-row space-y-4 sm:justify-between sm:items-center mb-6">
+            <div>
+              <h1 className="text-2xl">Showing all products</h1>
+              <p className="text-sm text-muted-foreground">
+                Fresh from the farm to your table
+              </p>
+            </div>
+            <div className="flex items-center gap-2">
+              <span className="text-sm">Sort by:</span>
+              <select
+                className="border rounded-md px-2 py-1 text-sm"
+                value={sortBy}
+                onChange={handleSortChange}
+              >
+                <option value="newest">Newest Products</option>
+                <option value="price-low-high">Price: Low to High</option>
+                <option value="price-high-low">Price: High to Low</option>
+                <option value="name-a-z">Name: A to Z</option>
+              </select>
+            </div>
           </div>
-          <div className="flex-1">
-            <ProductGrid
-              filterProducts={filterProducts.records}
-              filters={appliedFilters}
-              sortBy={sortBy}
-              onAddToCart={() => {
-                /* Add your onAddToCart logic here */
-              }}
-            />
-            <div className="mt-8">
-              <Pagination
-                pageCount={
-                  filterProducts?.count
-                    ? Math.ceil(filterProducts.count / limit)
-                    : 1
-                }
-                handlePageClick={(selectedPage) => {
-                  setPage(selectedPage.selected);
-                }}
+
+          <div className="flex flex-col lg:flex-row gap-6">
+            <div className="w-full lg:w-64 shrink-0 border px-6 pt-6 pb-20 rounded-md">
+              <FilterSidebar
+                filters={pendingFilters}
+                onFilterChange={setPendingFilters}
               />
+              <Button onClick={applyFilters} className="w-full mt-4">
+                Apply Filters
+              </Button>
+            </div>
+            <div className="flex-1">
+              <ProductGrid
+                filterProducts={filterProducts.records}
+                filters={appliedFilters}
+                sortBy={sortBy}
+              />
+              <div className="mt-8">
+                <Pagination
+                  pageCount={
+                    filterProducts?.count
+                      ? Math.ceil(filterProducts.count / limit)
+                      : 1
+                  }
+                  handlePageClick={(selectedPage) => {
+                    setPage(selectedPage.selected);
+                  }}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <Footer menuItems={menuItems} />
+        <Footer menuItems={menuItems} />
     </>
   );
 }
--- src/features/store/pages/StoreFront.tsx
+++ src/features/store/pages/StoreFront.tsx
@@ -11,58 +11,58 @@ import ProductCategoryGrid from "../components/ProductCategoryGrid";
 import Footer from "@/components/ui/footer";
 import { deliveryDays, menuItems, storeMenuItems } from "@/core/data";
 import { useStore } from "../hooks/useStore";
-import OverlayLoader from "@/components/ui/overlayloading";
+import InlineLoader from "@/components/ui/inlineloading";
 
 export default function StoreFront() {
   const { loading, popularProducts, categories } = useStore();
 
+  if (loading) {
+    return <InlineLoader loading={loading} children={<div></div>} />;
+  }
+
   return (
     <div className="pt-14 md:pt-16">
       <Header menuItems={storeMenuItems} />
-      <OverlayLoader loading={loading}>
-        <div>
-          <Banner />
-          <div className="bg-black py-4">
-            <Marquee speed={50}>
-              {deliveryDays.map((item, index) => (
-                <DeliveryDayComp
-                  key={index}
-                  location={item.location}
-                  dayOfWeek={item.dayOfWeek}
-                />
-              ))}
-            </Marquee>
-          </div>
-
-          <section className="bg-yellow-50 py-10">
-            {!loading && (
-              <ProductCategoryGrid
-                categories={categories}
-                selectCategory={function (_: ProductCategory): void {}}
+      <div>
+        <Banner />
+        <div className="bg-black py-4">
+          <Marquee speed={50}>
+            {deliveryDays.map((item, index) => (
+              <DeliveryDayComp
+                key={index}
+                location={item.location}
+                dayOfWeek={item.dayOfWeek}
               />
-            )}
-          </section>
-
-          <main className="container mx-auto px-4 py-8">
-            {!loading && (
-              <section className="text-center mt-8 md:mt-16">
-                <div className="text-brand-600 mb-2">Popular Choices</div>
-                <h2 className="text-2xl font-semibold mb-8">
-                  Popular Products
-                </h2>
-                <ProductGrid products={popularProducts.records} />
-                <div className="flex justify-center mt-12">
-                  <Button className="px-8 py-3 bg-brand-700 text-white rounded-md hover:bg-brand-600 transition-colors">
-                    <Link to={paths.store.listing.path}>View all Products</Link>
-                  </Button>
-                </div>
-              </section>
-            )}
-          </main>
-
-          <Footer menuItems={menuItems} />
+            ))}
+          </Marquee>
         </div>
-      </OverlayLoader>
+
+        <section className="bg-yellow-50 py-10">
+          {!loading && (
+            <ProductCategoryGrid
+              categories={categories}
+              selectCategory={function (_: ProductCategory): void {}}
+            />
+          )}
+        </section>
+
+        <main className="container mx-auto px-4 py-8">
+          {!loading && (
+            <section className="text-center mt-8 md:mt-16">
+              <div className="text-brand-600 mb-2">Popular Choices</div>
+              <h2 className="text-2xl font-semibold mb-8">Popular Products</h2>
+              <ProductGrid products={popularProducts.records} />
+              <div className="flex justify-center mt-12">
+                <Button className="px-8 py-3 bg-brand-700 text-white rounded-md hover:bg-brand-600 transition-colors">
+                  <Link to={paths.store.listing.path}>View all Products</Link>
+                </Button>
+              </div>
+            </section>
+          )}
+        </main>
+
+        <Footer menuItems={menuItems} />
+      </div>
     </div>
   );
 }
