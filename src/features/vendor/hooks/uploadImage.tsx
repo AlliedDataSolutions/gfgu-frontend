492c3846c4a6ecc074ddccb3ba756a3dca948725
Nagaraj Rotti <nagaraj.rotti@lethbridgecollege.ca>
2025-03-24 11:00:56 -0600

    fix: modified image upload hook to handle image uploads via Cloudinary

---
+++ src/features/vendor/hooks/uploadImage.tsx
@@ -0,0 +1,21 @@
+import axiosInstance from "@/core/axiosInstance";
+import { handleAxiosError } from "@/lib/handleAxiosError";
+
+interface ImageUploadResponse {
+  url : string;
+  secure_url : string;
+  asset_id: string;
+}
+
+
+  export const uploadImage = async (imageData:FormData): Promise< ImageUploadResponse | null> => {
+    try {
+      const response = await axiosInstance.post(`https://api.cloudinary.com/v1_1/da1ekxybq/upload`, {
+        data: imageData,
+      });
+      return response.data; // Returning product details
+    } catch (error) {
+      handleAxiosError(error);
+      return null;
+    }
+  };
