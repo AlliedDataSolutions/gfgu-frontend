a3257867cbbcebc142613976010f3ca80dd27599
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-03-27 06:28:34 -0600

    partially resize product card

--- src/features/store/components/ProductCard.tsx
+++ src/features/store/components/ProductCard.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import Apple from "@/assets/apple.png";
 import { paths } from "@/config/paths";
+import { Link } from "react-router-dom";
 
 interface ProductCardProps {
   product: Product;
@@ -23,21 +24,30 @@ export default function ProductCard({
 
   return (
     <div
-      className="overflow-hidden w-[160px] h-[350px] sm:w-[200px] sm:h-[370px] md:w-[230px]"
       onClick={(e) => {
         if ((e.target as HTMLElement).closest("button")) {
           return; // Prevent navigation if a button is clicked
         }
         handleNavigation(); // Navigate only if it's not a button click
       }}
+      className="overflow-hidden w-[160px] sm:w-[200px] md:w-[230px]"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="relative rounded-lg placeholder:aspect-square bg-neutral-50">
         <img
-          src={product.images?.length ? product.images[0].url : Apple}
+          src={
+            product.images?.length
+              ? product.images[0].url.replace(
+                  "/image/upload/",
+                  "/image/upload/w_200,h_200,c_fill/"
+                )
+              : ""
+          }
           alt={product.name}
-          className="object-cover w-full h-full p-6"
+          width="200"
+          height="200"
+          className="object-cover w-full h-full"
         />
         {isHovered && (
           <div className="w-full mt-2 absolute inset-0 flex items-end justify-center z-50 transition-opacity">
@@ -45,7 +55,7 @@ export default function ProductCard({
               onClick={(e) => {
                 e.stopPropagation();
                 if (onClickAddToCart) {
-                  onClickAddToCart(); // Ensure the function executes
+                  onClickAddToCart();
                 }
               }}
               className="w-full py-2 rounded-t-none bg-black hover:bg-black text-white"
@@ -55,7 +65,7 @@ export default function ProductCard({
           </div>
         )}
       </div>
-      <div className="p-1 flex flex-col items-center">
+      <div className="p-1 flex flex-col items-center h-[100px]">
         <h3 className="font-normal mb-1">{product.name}</h3>
         <p className="text-sm text-center text-neutral-600 mb-1">
           Sold By Farmer {product.vendor.businessName}
