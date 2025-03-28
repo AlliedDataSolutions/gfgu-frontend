222df8f48962e289a53d6560270367a46e084e78
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-03-28 14:19:46 -0600

    small changes

--- src/core/AddressType.ts
+++ src/core/AddressType.ts
@@ -3,3 +3,16 @@ export enum AddressType {
   shipping = "shipping",
   isUserAddress = "isUserAddress",
 }
+
+export const AddressLabel = (addressType: AddressType) => {
+  switch (addressType) {
+    case AddressType.billing:
+      return "Billing";
+    case AddressType.shipping:
+      return "Shipping";
+    case AddressType.isUserAddress:
+      return "Home Address";
+    default:
+      break;
+  }
+};
--- src/features/customer/components/AddressCard.tsx
+++ src/features/customer/components/AddressCard.tsx
@@ -1,9 +1,9 @@
-import { AddressType } from "@/core/AddressType";
+import { AddressLabel, AddressType } from "@/core/AddressType";
 import React from "react";
 
 interface Address {
   id: string;
-  addressType: string;
+  addressType: AddressType;
   streetName: string;
   town: string;
   province: string;
@@ -17,16 +17,15 @@ interface AddressCardProps {
 }
 
 const AddressCard: React.FC<AddressCardProps> = ({ address }) => {
-  const addressLabel =
-    address.addressType === "shipping" ? "Shipping Address" : "Billing Address";
+  const addressLabel = AddressLabel(address.addressType);
 
   return (
     <div className="bg-white p-6 mb-4 md:mb-0">
-      <h3 className="text-lg leading-6 font-medium text-gray-900">
+      <h3 className="text-base leading-6 font-medium text-gray-900">
         {addressLabel}
       </h3>
       <div className="mt-2 text-sm text-gray-500">
-        <p className="font-semibold">{address.streetName}</p>
+        <p>{address.streetName}</p>
         <p>
           {address.town}, {address.province} {address.postalCode}
         </p>
