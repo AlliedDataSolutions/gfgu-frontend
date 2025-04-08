8f91e89f075b2d8935f437ecc7146e65f24e183e
Adegbite femi <40705708+Efhemo@users.noreply.github.com>
2025-04-08 09:25:50 -0600

    add merchant id

--- src/features/customer/components/CheckoutButton.tsx
+++ src/features/customer/components/CheckoutButton.tsx
@@ -7,17 +7,22 @@ interface CheckoutProps {
 
 const Checkout: React.FC<CheckoutProps> = ({ amount }) => {
   const clientID = import.meta.env.VITE_REACT_APP_PAYPAL_CLIENT_ID || "";
+  const merchantId = import.meta.env.VITE_REACT_APP_ADMIN_PAYPAL_EMAIL || ""
   return (
-    <PayPalScriptProvider options={{ clientId: clientID }}>
+    <PayPalScriptProvider options={{ 
+      clientId: clientID, 
+      currency: "CAD",
+      merchantId: merchantId,
+       }}>
       <PayPalButtons
         createOrder={async () => {
-          const res = await axiosInstance.post("/api/paypal/create-order", {
+          const res = await axiosInstance.post("/payment/init", {
             amount,
           });
           return res.data.id;
         }}
         onApprove={async (data) => {
-          const res = await axiosInstance.post("/api/paypal/capture-payment", {
+          const res = await axiosInstance.post("/payment/capture-payment", {
             orderID: data.orderID,
           });
           console.log("Payment successful", res.data);
